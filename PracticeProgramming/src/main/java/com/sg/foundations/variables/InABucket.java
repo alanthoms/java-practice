06159fdeeb69e9898a058fa7252f34fdefccb91b
Alan Thomas <alanthomas171@gmail.com>
2026-04-23 15:22:02 +0100

    Add/ InABucket practice task

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/InABucket.java
@@ -0,0 +1,17 @@
+package com.sg.foundations.variables;
+
+public class InABucket {
+
+    public static void main(String[] args) {
+        String walrus;
+        double piesEaten;
+        float weightOfTeacupPig;
+        int grainsOfSand;
+
+        walrus = "Leroy Jenkins the Third";
+        piesEaten = 42.1;
+        System.out.println("Pet walrus, " + walrus );
+        System.out.println("He was hungry and ate this many pies : ");
+        System.out.println(piesEaten);
+    }
+}
