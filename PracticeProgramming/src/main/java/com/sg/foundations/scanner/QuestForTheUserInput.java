cdc1265386e84ddd2acf1389d530685241fe1b9b
Alan Thomas <alanthomas171@gmail.com>
2026-04-23 15:33:04 +0100

    Add/ QuestForTheUserInput practice task

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/scanner/QuestForTheUserInput.java
@@ -0,0 +1,25 @@
+package com.sg.foundations.scanner;
+import java.util.Scanner;
+public class QuestForTheUserInput {
+    public static void main(String[] args) {
+
+        Scanner inputReader = new Scanner(System.in);
+        String yourName;
+        String yourQuest;
+        double velocityOfSwallow;
+
+        System.out.println("Whats ur name");
+        yourName = inputReader.nextLine();
+
+        System.out.println("whats ur quest");
+        yourQuest = inputReader.nextLine();
+
+
+        System.out.println("What is the airspeed velocity of an unladen swallow?!?! ");
+        velocityOfSwallow = Double.parseDouble(inputReader.nextLine());
+        System.out.println();
+        System.out.println("How do you know " + velocityOfSwallow + " is correct, " + yourName + "?");
+        System.out.println("You didn't even know if the swallow was African or European!");
+        System.out.println("Maybe skip answering things about birds and instead go " + yourQuest + ".");
+    }
+}
