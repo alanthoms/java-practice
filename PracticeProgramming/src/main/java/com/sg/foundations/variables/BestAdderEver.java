b2f192893931a8316c4d6ee82de34a61f93e39c3
Alan Thomas <alanthomas171@gmail.com>
2026-04-23 15:25:37 +0100

    Add/ BestAdderEver practice task

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,12 @@
+package com.sg.foundations.variables;
+
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int int1 = 1;
+        int int2 = 2;
+        int int3 = 3;
+        int total = int1 + int2 + int3;
+
+        System.out.println("Math statement: " + int1 +" + " + int2 + " + " + int3 +" = " + total  );
+    }
+}
