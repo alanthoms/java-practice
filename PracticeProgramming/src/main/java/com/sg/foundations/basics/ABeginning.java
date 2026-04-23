642d49f8872df3521648b9dd524c18ba07e30c89
Alan Thomas <alanthomas171@gmail.com>
2026-04-23 15:02:23 +0100

    Initial commit

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ABeginning.java
@@ -0,0 +1,15 @@
+package com.sg.foundations.basics;
+
+public class ABeginning {
+
+    public static void main(String[] args){
+        System.out.println("Hello worlds");
+        System.out.println("Helly from the guild");
+        System.out.println("okay");
+        System.out.println("I think this is enough");
+        System.out.println("You can type sout and then tab so");
+        System.out.println("you dont need to type ");
+        System.out.println("System.out.println everytime");
+        System.out.println("But you can if you want");
+    }
+}
